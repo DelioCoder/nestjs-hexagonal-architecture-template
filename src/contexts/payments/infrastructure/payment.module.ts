48c6d1ae577b26c819b93967a96b4a52aa11136c
DelioCoder <davideveriwhere@gmail.com>
2025-09-21 21:47:21 -0500

    FindPaymentByIdUseCase added

--- src/contexts/payments/infrastructure/payment.module.ts
+++ src/contexts/payments/infrastructure/payment.module.ts
@@ -3,17 +3,19 @@ import { PaymentController } from "./controller/payment-controller";
 import { CreatePaymentUseCase } from "../application/create-payment-use-case";
 import { InMemoryPaymentRepository } from "./database/in-memory/in-memory-repository";
 import { PaymentRepository } from "../domain/payment.repository";
+import { FindPaymentByIdUseCase } from "../application/find-payment-by-id-use-case";
 
 @Module({
     controllers: [PaymentController],
     providers: [
         CreatePaymentUseCase,
+        FindPaymentByIdUseCase,
         InMemoryPaymentRepository,
         {
             provide: PaymentRepository,
             useExisting: InMemoryPaymentRepository,
         },
     ],
-    exports: [ CreatePaymentUseCase ]
+    exports: [ CreatePaymentUseCase, FindPaymentByIdUseCase ]
 })
 export class PaymentModule { }
